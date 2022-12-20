d00c9bfdfbbbe41f52e331de7f531dd34eab0759
fran-bnd <fran-bnd@hotmail.com>
2022-12-20 17:16:24 +0000

    Saved progress on Comparisons 2022-12-20 17:16:24 +0000

--- Comparisons/src/main/java/Comparators.java
+++ Comparisons/src/main/java/Comparators.java
@@ -53,16 +53,24 @@ public class Comparators {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+        if (a >= b){
+            return true;
+        }else{
+            return false;
+        }
     }
-    
+
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+        if (a <= b){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     /**
@@ -71,6 +79,10 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
-        return false;
+        if (a != b){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
