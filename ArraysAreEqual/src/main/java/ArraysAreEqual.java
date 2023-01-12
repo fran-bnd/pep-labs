f9323102246302a2adfbc35f26b9fc492cdda2bf
fran-bnd <fran-bnd@hotmail.com>
2023-01-12 03:14:10 +0000

    Saved progress on ArraysAreEqual 2023-01-12 03:14:10 +0000

--- ArraysAreEqual/src/main/java/ArraysAreEqual.java
+++ ArraysAreEqual/src/main/java/ArraysAreEqual.java
@@ -15,6 +15,12 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
-        return false;
+        boolean isEqual = true;
+        for (int i=0; i<a.length; i++){
+            if (a[i] != b[i]){
+                isEqual = false;
+            }
+        }
+        return isEqual;
     }
 }
