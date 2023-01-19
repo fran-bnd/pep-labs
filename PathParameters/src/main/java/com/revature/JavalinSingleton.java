6c4f171f9ab80dafbdb6ef3bd6a12866b2bfd37e
fran-bnd <fran-bnd@hotmail.com>
2023-01-19 21:46:57 +0000

    Saved progress on PathParameters 2023-01-19 21:46:57 +0000

--- PathParameters/src/main/java/com/revature/JavalinSingleton.java
+++ PathParameters/src/main/java/com/revature/JavalinSingleton.java
@@ -16,10 +16,11 @@ public class JavalinSingleton {
          */
         app.get("/firstname/{first}", ctx -> {
             
-            //write code here
+            String firstName = ctx.pathParam("first");
+
+            ctx.result(firstName);
 
         });
-
 
         return app;
     }
