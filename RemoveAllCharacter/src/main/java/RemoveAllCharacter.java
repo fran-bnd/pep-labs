6f07aa717400216ed6548d48d76b23347875ca7d
fran-bnd <fran-bnd@hotmail.com>
2023-01-28 01:44:34 +0000

    Saved progress on RemoveAllCharacter 2023-01-28 01:44:34 +0000

--- RemoveAllCharacter/src/main/java/RemoveAllCharacter.java
+++ RemoveAllCharacter/src/main/java/RemoveAllCharacter.java
@@ -12,6 +12,9 @@ public class RemoveAllCharacter {
      * @return str, without any characters of type ch.
      */
     public String removeAll(String str, String ch){
-        return null;
+
+        String modifiedString = str.replaceAll(ch, "");
+        
+        return modifiedString;
     }
 }
