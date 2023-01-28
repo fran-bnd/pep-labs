243e84f832ee19a8b961c9e5feaf372be44b475d
fran-bnd <fran-bnd@hotmail.com>
2023-01-28 02:22:15 +0000

    Saved progress on WordCount 2023-01-28 02:22:15 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,6 +5,12 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        int countWords = 0;
+        // char position = in.charAt(0);
+        for (int i=0; i < in.length(); i++){
+            //position = in.charAt(i);
+            if (!Character.isLetter(in.charAt(i))) countWords ++; 
+        }
+        return countWords+1;
     }
 }
