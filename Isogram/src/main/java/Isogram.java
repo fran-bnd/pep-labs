f94d443507d48cae3518d600cbc69d4a6089d179
fran-bnd <fran-bnd@hotmail.com>
2023-01-29 21:03:27 +0000

    Saved progress on Isogram 2023-01-29 21:03:27 +0000

--- Isogram/src/main/java/Isogram.java
+++ Isogram/src/main/java/Isogram.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Isogram {
     /**
@@ -13,6 +15,12 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        Set<Character> setOfChars = new HashSet<>();
+        for (int i=0; i < str.length(); i++){
+            char chars = Character.toLowerCase(str.charAt(i));
+            if (setOfChars.contains(chars)) return false;
+            setOfChars.add(chars);
+        }
+        return true; 
     }
 }
