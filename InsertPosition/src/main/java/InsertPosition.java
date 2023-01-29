7d36aa97dbba33b1c5159fe99f578be1114f0bc3
fran-bnd <fran-bnd@hotmail.com>
2023-01-29 21:35:59 +0000

    Saved progress on InsertPosition 2023-01-29 21:35:59 +0000

--- InsertPosition/src/main/java/InsertPosition.java
+++ InsertPosition/src/main/java/InsertPosition.java
@@ -15,6 +15,13 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        //int[] newArray = new int[nums.length +1];
+        int indexToInsert = 0;
+        for (int i=1 ; i < nums.length ; i++){
+            if (nums[0] > target) indexToInsert = 0;
+            if (nums[i-1] < target && target < nums[i]) indexToInsert = i;
+            if (nums[nums.length-1] < target) indexToInsert = nums.length;
+        }
+        return indexToInsert;
     }
 }
